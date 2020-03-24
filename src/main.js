e1376c0847e29f9566a7d6098484038e3e3c5449
huangjie <1198120989@qq.com>
2020-03-24 20:42:39 +0800

    Update main.js
    第一次提交

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+// vue构造函数
 import Vue from 'vue'
+//app组件（以vue结尾的文件都是组件）
 import App from './App.vue'
+//路由对象
 import router from './router'
-
+//上线环境是否提示信息
 Vue.config.productionTip = false
 
 new Vue({
